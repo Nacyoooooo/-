a5c1dedf207997797503edabfa1997a27ea112b8
jianhao <1467314512@qq.com>
2024-03-19 01:43:49 +0800

    xjh提交测试

--- src/main/java/com/jieduixiangmu/helloworld.java
+++ src/main/java/com/jieduixiangmu/helloworld.java
@@ -2,6 +2,8 @@ package com.jieduixiangmu;
 
 public class helloworld {
     public static void main(String[] args) {
+
         System.out.println("hello GraalVM!");
+        System.out.println("1+1=2");
     }
 }
